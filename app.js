1db2c13a85cd56821910f48bb4c0f95ff2c8478c
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2023-08-14 23:21:21 +0100

    added semi colon rule

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-'use strict'
-global.__basedir = __dirname
-const config = require('./config/config.js')
-const express = require('express')
-const app = express()
+'use strict';
+global.__basedir = __dirname;
+const config = require('./config/config.js');
+const express = require('express');
+const app = express();
 
 app.get('/health-check', (req, res) => {
-    return res.json({ status: 'api running smoothly' })
-})
+    return res.json({ status: 'api running smoothly' });
+});
 
 app.listen(config.PORT, config.HOST, () => {
-    console.log(`Server started and running at port ${config.PORT}`)
-})
+    console.log(`Server started and running at port ${config.PORT}`);
+});
--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config({
     path: __basedir + `/${process.env.APP_NODE_ENV}.env`,
 });
 
 module.exports = {
-    NODE_ENV: process.env.NODE_ENV || "development",
-    HOST: process.env.HOST || "localhost",
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    HOST: process.env.HOST || 'localhost',
     PORT: process.env.PORT || 3000,
 };
