5847125b60487464b1fa86c7b07bef06eb75b11f
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2023-08-15 21:17:17 +0100

    added sequelize

--- config/config.js
+++ config/config.js
@@ -8,4 +8,9 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || 'localhost',
     PORT: process.env.PORT || 3000,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_DATABASE: process.env.DB_DATABASE,
+    DB_HOST: process.env.DB_HOST,
+    DB_DIALECT: process.env.DB_DIALECT,
 };
