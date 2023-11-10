e8bf439b73f31186e46d0b40d5e3090c50c7487c
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2023-11-11 00:05:55 +0100

    removed association

--- models/user.js
+++ models/user.js
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            User.hasMany(Event, { as: 'events' });
         }
     }
     User.init(
